07126aafc167e8674190ff8e8a133c4875be7730
Dev Mittal <devmittal0407@gmail.com>
2022-11-09 11:48:14 +0530

    DB Connection Updated

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json())
 
 
 // DB Setup
-mongoose.connect('mongodb://localhost/blend')
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.once('open', () => console.log("Connected to Database"))
